aa4f5f84c339460e75d906eec1e37ca12961bf61
Raisa <volovik.raisa.kam@gmail.com>
2024-11-12 20:42:30 +0200

    added ProductService and IProductRepository

---
+++ FurniRoomStore/FurniRoomStore/Interfaces/IProductRepository.cs
@@ -0,0 +1,33 @@
+﻿using FurniRoomStore.Models;
+using FurniRoomStore.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace FurniRoomStore.Interfaces
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+        Task<byte[]> GetImageAsync(int productId);
+        Task UpdateImageAsync(int productId, byte[] imageData);
+    }
+
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        public ProductRepository(FurniRoomStoreContext context) : base(context) { }
+
+        public async Task<byte[]> GetImageAsync(int productId)
+        {
+            var product = await _dbSet.FindAsync(productId);
+            return product?.ImageData;
+        }
+
+        public async Task UpdateImageAsync(int productId, byte[] imageData)
+        {
+            var product = await _dbSet.FindAsync(productId);
+            if (product != null)
+            {
+                product.ImageData = imageData;
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
